6e4c1007d8d2d62d0512bc5ad559864f765d48dd
Scott Silverman <scottms724@gmail.com>
2019-09-30 14:59:26 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,3 @@
 class Post < ActiveRecord::Base
   belongs_to :category 
-  
-  def user_params
-    params.require(:post).permit(:username, :email, :password, :salt, :encrypted_password)
-  end
 end
